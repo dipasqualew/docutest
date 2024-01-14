90414fb0e3524e525fa0ae7dfcb55b13f077c495
William Di Pasquale <wdp@dipasqualew.com>
2024-01-14 15:08:57 +0000

    Support nested paths for the generated documents
    Fixes #2

--- packages/dt-vitest/fixtures/MagicWandWithExpectations.test.ts
+++ packages/dt-vitest/fixtures/MagicWandWithExpectations.test.ts
@@ -2,14 +2,14 @@ import { describe, test, beforeAll } from "vitest";
 import { annotateTest } from "../src/reporter";
 
 beforeAll((suite) => {
-    suite.meta.title = "Magic Wand (With Expectations)";
+    suite.meta.title = "Magic Wands/With Expectations";
     suite.meta.description = `
         The Magic Wand is the first item that players can win
         in their quest for magic.
     `;
 });
 
-describe("Magic Wand", () => {
+describe("Magic Wand With Expectations", () => {
 
     test("Sends magics to the enemies", async ({ task }) => {
         const expect = await annotateTest(task, `
--- packages/dt-vitest/vitest-extension.ts
+++ packages/dt-vitest/vitest-extension.ts
@@ -3,6 +3,7 @@ import "vitest"
 declare module 'vitest' {
     interface TaskMeta {
         title?: string;
+        docutest__path?: string;
         description?: string;
         expectations?: string[];
     }
